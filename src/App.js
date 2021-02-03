2c83a04a3496d0f075fdfb215cd9ba01514f149a
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-02-03 12:45:46 +0530

    add auth form and logic

--- src/App.js
+++ src/App.js
@@ -1,45 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import axios from "axios"
+
+// function App() {
+//   const getData = ()=>{
+//     var config = {
+//       method: 'get',
+//       url: 'http://localhost:3000/getdata',
+//       headers: { 
+        
+//       }
+//     };
+    
+//     axios(config)
+//     .then(function (response) {
+//       console.log(JSON.stringify(response.data));
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+    
+//   }
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save sdfsfsdfsdfsfsfsdf to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//       <button onClick={()=>getData()}>Get Data</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react"
 import axios from "axios"
+import LoginForm from "./components/LoginForm"
 
-function App() {
-  const getData = ()=>{
-    var config = {
-      method: 'get',
-      url: 'https://backic.azurewebsites.net/getdata',
-      headers: { 
-        'Cookie': 'ARRAffinity=c228d005896ab37d8c410789119641a5a5a86af4143695c1ab7878064c764896; ARRAffinitySameSite=c228d005896ab37d8c410789119641a5a5a86af4143695c1ab7878064c764896'
-      }
-    };
-    
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    
-  }
+
+const App = ()=>{
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save sdfsfsdfsdfsfsfsdf to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button onClick={()=>getData()}>Get Data</button>
-    </div>
-  );
+    <LoginForm />
+  )
 }
-
-export default App;
+export default App
---
+++ src/components/LoginForm.js
@@ -0,0 +1,46 @@
+import React,{useState} from "react"
+import axios from "axios"
+
+const LoginForm = (props)=>{
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+
+    const handleSubmit = (event)=>{
+        event.preventDefault()
+        var data = JSON.stringify({"username":username,"password":password});
+
+        var config = {
+        method: 'post',
+        url: 'http://localhost:3000/authuser',
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log("gkfhklfjgdkfjg error ererooer")
+        console.log(error);
+        });
+
+      }
+
+    return (
+        <form>
+        <label>
+            username:
+            <input type="text" name="username" value={username} onChange={(event)=>setUsername(event.target.value)}/>
+        </label>
+        <label>
+            password:
+            <input type="text" name="password" value={password} onChange={(event)=>setPassword(event.target.value)}/>
+        </label>
+        <input type="submit" value="Submit" onClick={(e)=>handleSubmit(e)}/>
+        </form>
+    )
+}
+export default LoginForm
