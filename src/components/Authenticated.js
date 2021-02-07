2b3c1e2183fdec171ea6e39216dc9f2e4ee974a1
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-02-07 15:01:56 +0530

    add style authenticated

--- src/components/Authenticated.js
+++ src/components/Authenticated.js
@@ -3,10 +3,11 @@ import GetContainers from "./GetContainers"
 const Authenticated = ()=>{
     return (
         <div className="loginform"> 
-        <div className="loginform__title">Authenticated Successfully!</div>
-        <div class>
+        <div className="loginform__title">Log In Successful</div>
+        <div>
             <GetContainers/>
         </div>
+        <div className="login__message"><div className="login__message__title login__message__title--top">Authenticated Successfully !</div></div>
         </div>
     )
 }
