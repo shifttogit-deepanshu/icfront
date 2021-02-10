12c04817adbcea1ddaf6fef7b136bd396973254c
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2021-02-10 13:05:12 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react"
 import LoginForm from "./components/LoginForm"
-import Containers from "./components/Containers"
 import {connect} from "react-redux"
-import TempChart from "./components/TempChart"
-import Map from "./components/ContainersMap"
-import TempMeterPos from "./components/TempMeterPos"
-import TempMeterNeg from "./components/TempMeterNeg.js"
 import Header from "./components/Header"
 import AppPort from "./components/AppPort"
 import Authenticated from "./components/Authenticated"
@@ -18,7 +13,7 @@ const App = (props)=>{
     <Header />
     <AppPort>
     {(props.uid===0 || props.uid===-1) && <LoginForm /> }
-    {(props.uid!==0 && props.uid!==-1 && Object.keys(props.containers).length==0) &&<Authenticated />}
+    {(props.uid!==0 && props.uid!==-1 && Object.keys(props.containers).length===0) &&<Authenticated />}
      
     
     {(props.uid!==0 && props.uid!==-1 && Object.keys(props.containers).length!==0) &&<AllContainers />}
